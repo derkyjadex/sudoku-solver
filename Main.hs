b32463e0c6e8c3041c3a393fc5cc41c67a01793d
James Deery <james.deery@jadex.org>
2015-02-13 23:00:27 +0000

    Calculate offset rather than using a case

--- Main.hs
+++ Main.hs
@@ -34,17 +34,7 @@ getRowValues board row =
 getBlockValues :: Board -> Int -> [Value]
 getBlockValues board block =
         let cells = [0, 1, 2, 9, 10, 11, 18, 19, 20]
-            offset = case block of
-                         0 -> 0
-                         1 -> 3
-                         2 -> 6
-                         3 -> 27
-                         4 -> 30
-                         5 -> 33
-                         6 -> 54
-                         7 -> 57
-                         8 -> 60
-                         _ -> undefined
+            offset = block `quot` 3 * 27 + (block `mod` 3 * 3)
             offsetCells = map (+offset) cells
          in map (board !!) offsetCells
 
